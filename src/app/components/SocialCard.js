93b161ea364fcda3a539500e54513153163e8acb
SaroltaBartos <sbartos03@gmail.com>
2025-05-07 16:31:11 +0100

    adding today's work

--- src/app/components/SocialCard.js
+++ src/app/components/SocialCard.js
@@ -10,3 +10,32 @@ const SocialCard = () => {
 }
 
 export default SocialCard
+
+
+
+//const image = imbb.com/12354325
+//<img src={`${image}`} className="col-start-1 row-span-3 h-[100px] w-[100px]"></img>
+
+
+/*
+import React from 'react'
+
+const Todoitem = ({title, completed, description, id, handleUpdate}) => {
+    return (
+        <div className={completed ? "bg-green-200" : "bg-red-200"}>
+            <h1>{title}</h1>
+            <p>{description}</p>
+            <p>{completed ? "Completed" : "Not Completed"}</p>
+            <button onClick={() => handleUpdate(id)}>
+                {completed ? "Mark as not completed" : "Mark as completed"}
+            </button>
+        </div>
+    )
+}
+
+export default Todoitem
+*/
+
+
+
+
