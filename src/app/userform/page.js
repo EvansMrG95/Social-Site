d7b489c87faf30ab8a36fd761529c5359f4151d7
EvansMrG95 <167923346+EvansMrG95@users.noreply.github.com>
2025-05-09 12:21:12 +0100

    user form changes

--- src/app/userform/page.js
+++ src/app/userform/page.js
@@ -46,49 +46,58 @@ const page = () => {
     }
 
   return (
-    <div className='flex flex-col'>
-        <h1 className='text-3xl text-center mb-10 text-white'>Add your product in the form below</h1>
-        <form onSubmit={handleSubmit} className='bg-gray-200 mx-auto w-[90%] md:w-[70%] p-5'>
-            <div className='grid grid-cols-2 justify-items-center space-y-5 mb-6'>
-                <div className='md:w-1/3'>
-                    <label className='text-gray-500 font-bold pr-4'>
-                            Business Name
-                    </label>
-                </div>
-                <div className='md:w-2/3'>
-                    <input name='businessName' placeholder='please enter the business name' onChange={handleInputChange} className='bg-gray-200 border-2 border-gray-200' />
-                </div>
-                <div className='md:w-1/3'>
-                    <label className='text-gray-500 font-bold pr-4'>
-                        Price
-                    </label>
-                </div>
-                <div className='md:w-2/3'>
-                    <input name='price' placeholder='enter the price' onChange={handleInputChange} className='bg-gray-200 border-2 border-gray-200' />
-                </div>                    
-                <div className='md:w-1/3'>
-                    <label className='text-gray-500 font-bold pr-4'>
-                        Description
-                    </label>
-                </div>
-                <div className='md:w-2/3'>
-                        <input name='description' placeholder='Product/Business description' onChange={handleInputChange} className='bg-gray-200 border-2 border-gray-200' />
-                </div>                   
-                <div className='md:w-1/3'>
-                    <label className='text-gray-500 font-bold pr-4'>
-                            Image URL
-                    </label>
-                </div>
-                <div className='md:w-2/3'>
-                    <input name='imageUrl' placeholder='please enter image of product' onChange={handleInputChange} className='bg-gray-200 border-2 border-gray-200' />
-                </div>
-                <button type="submit" className='cursor-pointer bg-blue-500 p-4'>
-                    Save business
-                </button>
-                <ToastContainer />
-            </div>
-        </form>
+    <div className='flex flex-col items-center py-10'>
+  <h1 className='text-3xl text-center mb-10 text-white'>Add your product in the form below</h1>
+  <form onSubmit={handleSubmit} className='bg-sky-200 shadow-lg rounded-lg w-[90%] md:w-[70%] p-8'>
+    <div className='grid grid-cols-1 gap-6'>
+      <div className='flex flex-col'>
+        <label className='text-gray-700 font-semibold mb-2'>Business Name</label>
+        <input
+          name='businessName'
+          placeholder='Please enter the business name'
+          onChange={handleInputChange}
+          className='p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400'
+        />
+      </div>
+      <div className='flex flex-col'>
+        <label className='text-gray-700 font-semibold mb-2'>Price</label>
+        <input
+          name='price'
+          placeholder='Enter the price'
+          onChange={handleInputChange}
+          className='p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400'
+        />
+      </div>
+      <div className='flex flex-col'>
+        <label className='text-gray-700 font-semibold mb-2'>Description</label>
+        <textarea
+          name='description'
+          placeholder='Product/Business description'
+          onChange={handleInputChange}
+          className='p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 min-h-[150px]'
+        />
+      </div>
+      <div className='flex flex-col'>
+        <label className='text-gray-700 font-semibold mb-2'>Image URL</label>
+        <input
+          name='imageUrl'
+          placeholder='Please enter image of product'
+          onChange={handleInputChange}
+          className='p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400'
+        />
+      </div>
+      <button
+        type="submit"
+        className='px-6 py-3 rounded-xl bg-cyan-500 text-white hover:bg-cyan-600 focus:outline-2 focus:outline-offset-2 focus:outline-sky-500 active:bg-sky-700'
+      >
+        Save Product
+      </button>
+
     </div>
+
+    <ToastContainer />
+  </form>
+</div>
   )
 }
 
